64db4392d8ad2c2454962a996744d975f5339389
Gabriel Pereyra <Gabriel@PC-Gabo.(none)>
2018-10-17 23:03:20 -0300

    Terminada carga de context y signatures

--- Entrega2/TypeChecker.hs
+++ Entrega2/TypeChecker.hs
@@ -22,11 +22,6 @@ type SignParameter = (Ident,(Bool,Type))
 typeCheck :: Program -> Err ()
 typeCheck program = checkProgram program
 
-
--- SE DEBE HACER DOS RECORRIDAS SUPUESTAMENTE, EN LA PRIMERA SE CARGA EL CONTEXT Y EL SIGNATURES Y
--- LUEGO CON EL ENV RESULTADO DE LA PRIMERA RECORRIDA SE COMIENZA A HACER LAS STMS
-
-
 -- CHECK Program
 checkProgram :: Program -> Err ()
 checkProgram (PBlock name varPart procFuns stms) = do {
@@ -34,6 +29,7 @@ checkProgram (PBlock name varPart procFuns stms) = do {
                                                         chekBodyAndFunctions procFuns stms env
                                                       }
 
+-- Realiza la carga del Env primero cargando el context y luego las signatures
 buildEnv :: VarPart -> [Def] -> Err ((Context,Signatures))
 buildEnv varPart procFuns = do {
                                  resultContext <- buildContext varPart Map.empty;
@@ -41,6 +37,8 @@ buildEnv varPart procFuns = do {
                                  return (resultContext,resultSignatures)
                                }
 
+-- Realiza la carga de las signatures y es llamada en forma recursiva.
+-- Por cada Function o Procedure carga su firma
 buildSignatures :: [Def] -> Signatures -> Err (Signatures)
 buildSignatures [] signatures = return (signatures)
 buildSignatures (d:ds) signatures = do {
@@ -48,13 +46,19 @@ buildSignatures (d:ds) signatures = do {
                                          buildSignatures ds resultSignatures
                                        }
 
+-- Realiza la carga de una signature individual
+-- Se distingue sobre si es Function o Procedure
 buildSignature :: Signatures -> Def -> Err (Signatures)
-buildSignature signatures (DProc name parms varPart stms) = return (Map.empty)
+buildSignature signatures (DProc name parms varPart stms) = do {
+                                                                 listSignParams <- checkListParams [] name parms;
+                                                                 return (addSignatureProc signatures name listSignParams)
+                                                               }
 buildSignature signatures (DFun name parms funType varPart stms) = do {
                                                                         listSignParams <- checkListParams [] name parms;
-                                                                        return (addSignature signatures name funType listSignParams)
+                                                                        return (addSignatureFun signatures name funType listSignParams)
                                                                       }
 
+-- Realiza la carga de los parametros de una signature
 checkListParams :: [SignParameter] -> Ident -> [Param] -> Err ([SignParameter])
 checkListParams sigParams name [] = return (sigParams);
 checkListParams sigParams name (p:ps) = do {
@@ -62,22 +66,30 @@ checkListParams sigParams name (p:ps) = do {
                                              checkListParams  resultSigParms name ps
                                            }
 
+-- Se distingue segun el tipo de parametro si es por referencia o no
 checkParams :: Param -> [SignParameter] -> Ident -> Err ([SignParameter])
-checkParams (ParamSingle [] t) sigParams name = return (sigParams)
-checkParams (ParamSingle (i:is) t) sigParams name = case lookup i sigParams of {
-                                                      (Just a) -> fail ("Variable " ++ show(i) ++ " ya se encuentra delacarada en la firma de " ++ show(name));
-                                                       Nothing -> return ((i,(False,t)):sigParams) -- SE AGREGAR A LA LISTA sigParams
-                                                    }
-checkParams (ParamRef [] t) sigParams name = return (sigParams)
-checkParams (ParamRef (i:is) t) sigParams name = case lookup i sigParams of {
-                                                      (Just a) -> fail ("Variable " ++ show(i) ++ " ya se encuentra delacarada en la firma de " ++ show(name));
-                                                       Nothing -> return ((i,(True,t)):sigParams) -- SE AGREGAR A LA LISTA sigParams
-                                                    }
+checkParams (ParamSingle [] t) signParams name = return (signParams)
+checkParams (ParamSingle (i:is) t) signParams name = addParamToListSignParameters (i:is) signParams name t
+checkParams (ParamRef [] t) signParams name = return (signParams)
+checkParams (ParamRef (i:is) t) signParams name = addParamToListSignParameters (i:is) signParams name t
 
-addSignature :: Signatures -> Ident -> Type -> [SignParameter] -> Signatures
-addSignature signatures name t listSignParams = signatures
+-- Realiza la carga de un parametro en la lista de parametros de la signature
+addParamToListSignParameters :: [Ident] -> [SignParameter] -> Ident -> Type -> Err ([SignParameter])
+addParamToListSignParameters [] signParams name t = return (signParams)
+addParamToListSignParameters (i:is) signParams name t = case lookup i signParams of {
+                                                                (Just a) -> fail ("Variable " ++ show(i) ++ " ya se encuentra delacarada en la firma de " ++ show(name));
+                                                                Nothing -> addParamToListSignParameters is (((i,(False,t)):signParams)) name t
+                                                            }
 
+-- Realiza la carga de una signature Procedure en la tabla de signatures
+addSignatureProc :: Signatures -> Ident -> [SignParameter] -> Signatures
+addSignatureProc signatures name listSignParams = Map.insert name (listSignParams,Nothing) signatures
 
+-- Realiza la carga de una signature Function en la tabla de signatures
+addSignatureFun :: Signatures -> Ident -> Type -> [SignParameter] -> Signatures
+addSignatureFun signatures name t listSignParams = Map.insert name (listSignParams,Just(t)) signatures
+
+-- Realiza la carga del Contexto
 buildContext :: VarPart -> Context -> Err (Context)
 buildContext VPartEmpty context = return (Map.empty)
 buildContext (VPart []) context = return (Map.empty)
@@ -86,6 +98,7 @@ buildContext (VPart (i:is)) context = do {
                                            buildContext (VPart is) resultContext;
                                          }
 
+-- Chequea si la variable ya no existe en el contexto, si no existe la agrega
 chkAddContext :: VarDecl -> Context -> Err (Context)
 chkAddContext (VDecl [] t) context = return context
 chkAddContext (VDecl (i:is) t) context = case Map.lookup i context of
@@ -94,13 +107,14 @@ chkAddContext (VDecl (i:is) t) context = case Map.lookup i context of
                                                                chkAddContext (VDecl is t) (Map.insert i t context);
                                                              }
 
-
+-- Realiza el chequeo de las statement del body de las Function y/o Procedures y del body del program
 chekBodyAndFunctions :: [Def] -> [Stm] -> Env -> Err ()
 chekBodyAndFunctions procFuns stms env = checkStms env stms
 
+-- Realiza el chequeo de una lista de statements
 checkStms :: Env -> [Stm] -> Err ()
 checkStms env stms = return ()
 
---checkStament :: Env -> Stm -> Err ()
+--checkstatement :: Env -> Stm -> Err ()
 --checkExp :: Env -> Exp -> Type -> Err ()
 --inferExp :: Env -> Exp -> Err (Type)
